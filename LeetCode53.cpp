b30ae3bfc410ec49c1bc55eb66b504d1910ef6e1
sandeepkolleti <sandeepkolleti1919@gmail.com>
2026-01-02 02:06:06 +0530

    LeetCode problem 53

---
+++ LeetCode53.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN,currentSum = 0;
+        for(int num : nums) {
+            currentSum += num;
+            if(currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            if(currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
